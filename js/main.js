892df07ff86c703e6f0c6a39b5f7399ec4422119
Sonale17! <verbitskiya@bk.ru>
2022-08-11 21:55:10 +0300

    scriptdddd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,51 @@
-const lazyContent = new LazyLoad({
-  use_native: true // <-- there you go
-});
+// const lazyContent = new LazyLoad({
+//   use_native: true, // <-- there you go
+// });
+
+(function () {
+  function logElementEvent(eventName, element) {
+    console.log(Date.now(), eventName, element.getAttribute("data-src"));
+  }
+
+  var callback_enter = function (element) {
+    logElementEvent("🔑 ENTERED", element);
+  };
+  var callback_exit = function (element) {
+    logElementEvent("🚪 EXITED", element);
+  };
+  var callback_loading = function (element) {
+    logElementEvent("⌚ LOADING", element);
+  };
+  var callback_loaded = function (element) {
+    logElementEvent("👍 LOADED", element);
+  };
+  var callback_error = function (element) {
+    logElementEvent("💀 ERROR", element);
+  };
+  var callback_finish = function () {
+    logElementEvent("✔️ FINISHED", document.documentElement);
+  };
+  var callback_cancel = function (element) {
+    logElementEvent("🔥 CANCEL", element);
+  };
+
+  var ll = new LazyLoad({
+    class_applied: "lz-applied",
+    class_loading: "lz-loading",
+    class_loaded: "lz-loaded",
+    class_error: "lz-error",
+    class_entered: "lz-entered",
+    class_exited: "lz-exited",
+    // Assign the callbacks defined above
+    callback_enter: callback_enter,
+    callback_exit: callback_exit,
+    callback_cancel: callback_cancel,
+    callback_loading: callback_loading,
+    callback_loaded: callback_loaded,
+    callback_error: callback_error,
+    callback_finish: callback_finish,
+  });
+})();
 
 $(".hamburger").click(function () {
   $(".header").toggleClass("active");
@@ -1074,7 +1119,10 @@ $(document).ready(function () {
   let prevScrollpos = window.pageYOffset;
   $(window).scroll(function (event) {
     lastScrollpos = window.pageYOffset;
-    if ((lastScrollpos > prevScrollpos + 30) || (lastScrollpos < prevScrollpos - 30)) {
+    if (
+      lastScrollpos > prevScrollpos + 30 ||
+      lastScrollpos < prevScrollpos - 30
+    ) {
       $(".header").removeClass("active");
       prevScrollpos = window.pageYOffset;
     }
